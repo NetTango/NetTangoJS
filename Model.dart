db86df76afc831f0dfa11d7af1209da3bd914178
Michael Horn <michael-horn@northwestern.edu>
2012-11-19 00:24:18 -0600

    Fixed part of convention; fixed resize function

--- Model.dart
+++ Model.dart
@@ -11,6 +11,8 @@
  * material are those of the author(s) and do not necessarily reflect the views
  * of the National Science Foundation (NSF).
  */
+part of nettango;
+
 abstract class Model {
    
    // Drawing context for turtles (TODO: use proper type)
@@ -63,7 +65,7 @@ abstract class Model {
    }
    
    
-   abstract void setup();
+   void setup();
    
    
    void initPatches() { 
@@ -117,6 +119,12 @@ abstract class Model {
       canvas.style.top = "${y}px";
 
       canvas = document.query("#turtles");
+      canvas.width = w;
+      canvas.height = h;
+      canvas.style.left = "${x}px";
+      canvas.style.top = "${y}px";
+      
+      canvas = document.query("#events");
       canvas.width = w;
       canvas.height = h;
       canvas.style.left = "${x}px";
