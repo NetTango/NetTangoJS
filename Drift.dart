aa091b0654da21d55e4c5f02da8e68ae5c0a5377
Michael Horn <michael-horn@northwestern.edu>
2013-02-28 17:27:48 -0600

    work in progress

--- Drift.dart
+++ Drift.dart
@@ -1,28 +1,23 @@
-library nettango;
-
 import 'dart:html';
 import 'dart:math';
 import 'dart:json';
-
-part 'Color.dart';
-part 'Touch.dart';
-part 'Turtle.dart';
-part 'Patch.dart';
-part 'Tween.dart';
-part 'Model.dart';
-part 'Toolbar.dart';
-part 'Button.dart';
-part 'PieChart.dart';
-part 'JsonObject.dart';
-part 'NetTango.dart';
+import 'core/ntango.dart';
 
 
 void main() {
-   DriftModel model = new DriftModel();
-   NetTango ntango = new NetTango(model);
-   ntango.showToolbar();
-   ntango.resizeToFitScreen();
-   ntango.restart();
+  DriftModel model = new DriftModel("Drift");
+  model.setup();
+  
+  window.onMessage.listen((event) {
+    /*
+    print("message: ${event.data}");
+    Interpreter interp = new Interpreter();
+    interp.load(event.data);
+    while (interp.step()) {
+      print('.');
+    }
+    */
+  });
 }
 
 
@@ -30,74 +25,74 @@ class DriftModel extends Model {
   
    final int TURTLE_COUNT = 60;
    
-   DriftModel() : super() {  }
+   DriftModel(String name) : super(name, 'drift') {  }
    
    
-   void setup() {
-      clearTurtles();
-      clearPatches();
-      initPatches();
+  void setup() {
+    clearTurtles();
+    clearPatches();
+    initPatches();
       
-      var colors = [
-                   new Color(255, 0, 0, 255),
-                   new Color(0, 255, 0, 255),
-                   new Color(0, 0, 255, 255),
-                   new Color(255, 255, 0, 255),
-                   new Color(0, 255, 255, 255)];
-     
-      for (int i=0; i<TURTLE_COUNT; i++) {
-         DriftTurtle t = new DriftTurtle(this);
-         t.color = colors[i % 5].clone();
-         addTurtle(t);
-      }
-   }
+    var colors = [
+                new Color(255, 0, 0, 255),
+                new Color(0, 255, 0, 255),
+                new Color(0, 0, 255, 255),
+                new Color(255, 255, 0, 255),
+                new Color(0, 255, 255, 255)];
+    
+    String behaviors = """
+    [
+      ["forward", 0.1],
+      ["right", ["random", 20] ],
+      ["left", ["random", 20] ],
+      ["set", "energy", ["-", "energy", 0.01] ],
+      ["ask", ["patch-here"], [ [ "set", "color-blue", 100 ], [ "set", "color-alpha", 125 ] ] ],
+      ["if", [ "<=", "energy", 0 ], [ "die" ] ],
+      ["if", [ ">", "energy", 0.9],
+        ["if", [ ">", ["random", 100], 95 ], [ ["set", "energy", 0.5 ], [ "hatch" ] ] ]
+      ]
+    ]
+    """;
+    Expression behavior = new Expression(parse(behaviors));
+    
+    
+    for (int i=0; i<TURTLE_COUNT; i++) {
+      Turtle t = new Turtle(this);
+      t["energy"] = 0.5 + Turtle.rnd.nextDouble() * 0.5;
+      t.color = colors[i % 5].clone();
+      t.setBehavior(behavior);
+      addTurtle(t);
+    }
+    
+    for (Patch patch in patches) {
+      patch["energy"] = 100;
+    }
+  }
 }
 
-
-class DriftTurtle extends Turtle {
-
-   num energy = 1;
-  
-   
-   DriftTurtle(Model model) : super(model) {
-      energy = 0.5 + Turtle.rnd.nextDouble() * 0.5;
-   }
-   
-   
-   void tick() {
-      forward(0.1);
-      right(Turtle.rnd.nextInt(20));
-      left(Turtle.rnd.nextInt(20));
-      Patch p = patchHere();
-      if (energy < 1 && p.energy > 0.2) {
-         energy += 0.03;
-         p.energy -= 0.2;
-      }
-      color.alpha = (255 * energy).toInt();
-      energy -= 0.01;
-      if (energy <= 0) {
+/*
+  void tick() {
+    forward(0.1);
+    right(Turtle.rnd.nextInt(20));
+    left(Turtle.rnd.nextInt(20));
+    Patch p = patchHere();
+      //if (energy < 1 && p.energy > 0.2) {
+      //   energy += 0.03;
+      //   p.energy -= 0.2;
+     // }
+    color.alpha = (255 * this["energy"]).toInt();
+    this["energy"] -= 0.01;
+    if (this["energy"] <= 0) {
          die();
-      } else if (energy > 0.9 && Turtle.rnd.nextInt(100) > 95) {
-         reproduce();
-      }
-   }
+    } else if (this["energy"] > 0.9 && Turtle.rnd.nextInt(100) > 95) {
+      reproduce();
+      
+    }
+  }
    
-   
-   bool touchDown(TouchEvent event) {
-      die();
-   }
-   
-   
-   void reproduce() {
-      DriftTurtle copy = new DriftTurtle(model);
-      copy.x = x;
-      copy.y = y;
-      copy.heading = heading;
-      copy.color.red = color.red;
-      copy.color.green = color.green;
       copy.color.blue = color.blue;
-      copy.energy = 0.5;
-      energy = 0.5;
+      copy["energy"] = 0.5;
+      this["energy"] = 0.5;
       model.addTurtle(copy);
    }
-}
+*/
