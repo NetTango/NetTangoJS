97ce1e06381aadf7efc998a01eddb8a70f76f712
Arthur Hjorth <arthur.hjorth@u.northwestern.edu>
2013-10-21 10:55:22 -0500

    changes to agentsetnew

--- core/agentsetnew.dart
+++ core/agentsetnew.dart
@@ -9,71 +9,54 @@
  */
 part of NetTango;
 
-class AgentSetNew implements Set<String>{
+class AgentSetNew implements Set<Turtle>{
   
-  Set<String> _set;
+  Set<Turtle> _set = new Set<Turtle>();
   
+  // standard getters/setters stuff
   int get length => _set.length;
+  bool get isEmpty => _set.isEmpty;
   
-  AgentSetNew(): _set = new Set<String>();
   
-  AgentSetNew.from(Iterable<String> e) : _set = new Set.from(e);
+  AgentSetNew.fromEmpty() {}
   
+  AgentSetNew.fromAgentSet(Iterable<Turtle> e) : _set = new Set.from(e);
+  
+  AgentSetNew.fromAgent(Turtle t)  
+  {
+    _set.add(t);
+  }
+    
   void removeAll(Iterable<Object> elements){
     _set.removeAll(elements);
   }
   
   void addAll(Iterable<Object> elements){
     _set.addAll(elements);
-  
-  Iterable<String> where(bool f (String element)){
+  }  
+  Iterable<Turtle> where(bool f (Turtle element)){
     return _set.where(f);
   }
   
+  AgentSetNew shuffleAndReturn() {
+    var random = new Random();
+
+    // Go through all elements.
+    for (var i = _set.length - 1; i > 0; i--) {
+
+      // Pick a pseudorandom number according to the list length
+      var n = random.nextInt(_set.length);
+
+      var temp = _set.elementAt(i);
+      _set[i] = items[n];
+      items[n] = temp;
+    }
+
+    return this;
+  }
+  
+
+  
+
+  
 }
-  
-
-
-main() {
-  String t = "hep";
-  String t2 = "hep";
-  String t3 = "hep3";
-
-  var tl = new List<String>();
-  
-  tl.addAll([t, t2, t3]);
-  var tl2 = new AgentSetNew.from(tl);
-
-  print(tl.length);
-  print(tl2.length);
-  
-  print(tl2.where((i) => i == 'hep').length);
-}
-
-  
-//  Set turtles = new Set<Turtle>();
-//  
-//  AgentSet.empty() {
-//    
-//  }
-//  AgentSet.fromAgentSet(AgentSet aset){
-//    
-//  }
-//  
-//  
-//  
-//  void add(Turtle t){
-//    turtles.add(t);
-//  }
-//  
-//  Turtle oneOf(){
-//    return turtles.elementAt(Turtle.rnd.nextInt(turtles.length));
-//  }
-//  
-//  AgentSet turtlesWith(var aTest){
-//    AgentSet newSet = AgentSet.fromAgentSet(turtles.where(aTest));
-//    return newSet;
-//    
-//  }
-//  
-  
